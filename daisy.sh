e17cec8ca69bb246e445b8ccd7f392f19c3aecd5
Dell Du <18588220928@163.com>
2020-12-25 20:23:35 +0800

    Update.

--- daisy.sh
+++ daisy.sh
@@ -1,9 +1,9 @@
 lightweight_gan \
     --data flower_photos/daisy \
     --name daisy \
-    --latent-dim 64 \
-    --attn-res-layers [8] \
-    --batch-size 64 \
+    --latent-dim 256 \
+    --attn-res-layers [32] \
+    --batch-size 32 \
     --amp \
     --gradient-accumulate-every 4 \
     --num-train-steps 200000
